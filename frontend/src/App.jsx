050a5cb051d043f59b95ff3acbc909b9efd7011f
khushi kumari <khushi8252kumari@gmail.com>
2026-07-24 03:32:41 +0530

    phase1 and phase2 configration:-talwind css & initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>hello khushi</h1>
+    </>
+  )
+}
+
+export default App
